76141f5a195cfb3c71a53a129d1543774db7bef1
Gerardo Miranda <gerardodev25@gmail.com>
2024-07-17 20:59:41 -0400

    chore(dev): createing create category dto

--- src/presentation/category/controller.ts
+++ src/presentation/category/controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { CustomError } from '../../domain/errors';
 import { CategoryService } from '../services/';
+import { CreateCategoryDto } from '../../domain/dtos/category';
 
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
@@ -14,9 +15,13 @@ export class CategoryController {
   }
 
   public createCategory = async (req: Request, res: Response) => {
-    return res.json({
-      message: 'Category created successfully',
-    });
+    const [error, createCategoryDto] = CreateCategoryDto.create(req.body);
+
+    if (error) {
+      return res.status(400).json({ error });
+    }
+
+    return res.json(createCategoryDto);
   };
 
   public getCategory = async (req: Request, res: Response) => {
