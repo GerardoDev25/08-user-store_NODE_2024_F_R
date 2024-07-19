e2decb46325c7f4bec6fc9fe7f9dbce9d0099784
Gerardo Miranda <gerardodev25@gmail.com>
2024-07-18 20:36:26 -0400

    chore(dev): creating create product dto

---
+++ src/domain/dtos/products/create-product.dto.ts
@@ -0,0 +1,30 @@
+export class CreateProductDto {
+  private constructor(
+    public readonly name: string,
+    public readonly available: boolean,
+    public readonly price: number,
+    public readonly description: string,
+    public readonly user: string,
+    public readonly category: string
+  ) {}
+
+  static create(object: { [key: string]: any }): [string?, CreateProductDto?] {
+    const { name, available, price, description, user, category } = object;
+
+    if (!name) return ['missing Name.', undefined];
+    if (!user) return ['missing user.', undefined];
+    if (!category) return ['missing category.', undefined];
+
+    return [
+      undefined,
+      new CreateProductDto(
+        name,
+        !!available,
+        price,
+        description,
+        user,
+        category
+      ),
+    ];
+  }
+}
